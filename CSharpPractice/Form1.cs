6b2c68284b40681a68b6d14eb81f9f8596c8fea3
Lee Kwenwon <highfence@naver.com>
2017-01-14 15:48:03 +0900

    Form 3 and adder

--- CSharpPractice/Form1.cs
+++ CSharpPractice/Form1.cs
@@ -17,25 +17,23 @@ namespace CSharpPractice
             InitializeComponent();
         }
 
-        string helloWord;
-        string loveWord;
-        string exitWord;
+        string helloWord = "안녕하세요";
+        string loveWord = "사랑합니다";
+        string exitWord; 
 
         private void button3_Click(object sender, EventArgs e)
         {
-            helloWord = "안녕하세요";
             MessageBox.Show(helloWord);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            loveWord = "사랑합니다.";
             MessageBox.Show(loveWord);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            exitWord = "나갈까요?";
+            exitWord = helloWord + " " + loveWord;
             MessageBox.Show(exitWord);
         }
     }
---
+++ CSharpPractice/Form3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpPractice
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        int intVal1;
+        int intVal2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != ""))
+            {
+                intVal1 = Convert.ToInt16(textBox1.Text);
+                intVal2 = Convert.ToInt16(textBox2.Text);
+                int resultVal = intVal1 + intVal2;
+
+                MessageBox.Show(resultVal.ToString());
+            }
+        }
+    }
+}
