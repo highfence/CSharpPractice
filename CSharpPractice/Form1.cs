f4ac8cb7a89194e88eec90f86c48a7a40a60e521
Lee Kwenwon <highfence@naver.com>
2017-01-14 14:58:51 +0900

    Make First Buttons and Event

--- CSharpPractice/Form1.cs
+++ CSharpPractice/Form1.cs
@@ -16,5 +16,10 @@ namespace CSharpPractice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요?");
+        }
     }
 }
